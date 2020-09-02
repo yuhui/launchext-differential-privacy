7a34d9ffea9eff9faa3eec5356fcced62a279488
Yuhui <yuhuibc@gmail.com>
2020-09-02 09:21:25 +0800

    feat: return a pre-selected item or a random item from a list to a data element

---
+++ src/lib/dataElements/differentialItem.js
@@ -0,0 +1,148 @@
+/**
+ * Copyright 2020 Yuhui. All rights reserved.
+ *
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+var log = require('../helpers/log');
+
+/**
+ * Returns the object's type using `Object.prototype.toString`.
+ * Reference: https://stackoverflow.com/a/332429
+ *
+ * @param {*} obj The object to get its type.
+ *
+ * @returns {string} The object's type.
+ */
+var objectType = function(obj) {
+  return Object.prototype.toString.call(obj).slice(8, -1);
+};
+
+/**
+ * The differential item data element.
+ *
+ * @param {Object} settings The data element settings object.
+ * @param {string or Array} settings.listOfPossibleItems A comma-separated
+ * string or a data element that returns a comma-separated string or an array
+ * of items.
+ * @param {string or Object} settings.selectedItem A string or a data element
+ * that returns an object.
+ * @param {float} settings.probabilityOfUsingSelectedItem A float (stored as a
+ * string) that represents the probability of returning the selected item.
+ * @param {string} settings.returnType "value" or "position".
+ *
+ * @returns {Object} An item from the array of items. This could be the
+ * selected item itself, if the probability is met, or a random item from the
+ * array. If any validation fails, a blank string is returned.
+ */
+module.exports = function(settings) {
+  // validate settings.selectedItem
+  var selectedItem = settings.selectedItem;
+  if (!selectedItem) {
+    log('error', 'missing selected item');
+    return;
+  }
+
+  // validate settings.probabilityOfUsingSelectedItem
+  var probabilityOfUsingSelectedItem = settings.probabilityOfUsingSelectedItem;
+  if (!probabilityOfUsingSelectedItem) {
+    log('error', 'missing probability of using selected item');
+    return;
+  }
+  var probabilityOfUsingSelectedItemType = objectType(
+    probabilityOfUsingSelectedItem
+  );
+  if (
+    ['Number', 'String'].indexOf(probabilityOfUsingSelectedItemType) === -1
+  ) {
+    log('error', 'probability of using selected item is not a number');
+    return;
+  }
+  if (probabilityOfUsingSelectedItemType === 'String') {
+    probabilityOfUsingSelectedItem = parseFloat(probabilityOfUsingSelectedItem);
+  }
+  if (!probabilityOfUsingSelectedItem) {
+    log('error', 'probability of using selected item is not a number');
+    return;
+  }
+  if (
+    probabilityOfUsingSelectedItem < 0.0 || probabilityOfUsingSelectedItem > 1.0
+  ) {
+    log(
+      'error',
+      'probability of using selected item must be between 0.0 and 1.0'
+    );
+    return;
+  }
+
+  // validate settings.listOfPossibleItems
+  var listOfPossibleItems = settings.listOfPossibleItems;
+  if (!listOfPossibleItems) {
+    log('error', 'missing list of possible items');
+    return;
+  }
+  var listOfPossibleItemsType = objectType(listOfPossibleItems);
+  if (['Array', 'String'].indexOf(listOfPossibleItemsType) === -1) {
+    log('error', 'list of possible items is not a string nor an array');
+    return;
+  }
+  if (listOfPossibleItemsType === 'String') {
+    listOfPossibleItems = listOfPossibleItems.split(',').map(function(i) {
+      return i.trim();
+    });
+  }
+
+  // validate settings.returnType
+  var returnType = settings.returnType;
+  if (!returnType) {
+    log('error', 'missing return type');
+    return;
+  }
+  if (!/^(position|value)$/.test(returnType)) {
+    log('error', 'return type is neither "position" nor "value"');
+    return;
+  }
+
+  // assume that the item to return is the selected item
+  var returnItem = selectedItem;
+
+  var randomNumber = Math.random();
+  if (randomNumber > probabilityOfUsingSelectedItem) {
+    // probability is not met
+    // get a random item from the list of possible items
+    var numPossibleItems = listOfPossibleItems.length;
+    var randomPosition = Math.floor(Math.random() * numPossibleItems);
+    returnItem = listOfPossibleItems[randomPosition];
+  }
+
+  var returnValue;
+  switch (returnType) {
+    case 'position':
+      // position is 1-based
+      // if the selected item is not in the list, then this returns 0,
+      // ...which is correct!
+      returnValue = listOfPossibleItems.indexOf(returnItem) + 1;
+      break;
+    case 'value':
+      returnValue = returnItem;
+  }
+  if (!returnValue && returnValue === 0) {
+    returnValue += '';
+  }
+  if (returnValue) {
+    log('info', 'returned: ' + returnValue);
+    return returnValue;
+  }
+};
---
+++ src/spec/dataElements/differentialItem.spec.js
@@ -0,0 +1,166 @@
+/**
+ * Copyright 2020 Yuhui. All rights reserved.
+ *
+ * Licensed under the GNU General Public License, Version 3.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+describe('differentialItem data element delegate', function() {
+  var dataElementDelegate = require('../../lib/dataElements/differentialItem');
+
+  beforeEach(function() {
+    this.settings = {
+      selectedItem: 'lorem',
+      probabilityOfUsingSelectedItem: '0.75',
+      listOfPossibleItems: ['foo', 'bar', 'stamp'],
+      returnType: 'value'
+    };
+  });
+
+  describe('with invalid "settings" argument', function() {
+    it(
+      'should return empty string when "selectedItem" is missing',
+      function() {
+        delete this.settings.selectedItem;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "probabilityOfUsingSelectedItem" is ' +
+      'missing',
+      function() {
+        delete this.settings.probabilityOfUsingSelectedItem;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "probabilityOfUsingSelectedItem" type ' +
+      'is invalid',
+      function() {
+        this.settings.probabilityOfUsingSelectedItem = true;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "probabilityOfUsingSelectedItem" is ' +
+      'not a number',
+      function() {
+        this.settings.probabilityOfUsingSelectedItem = 'abc';
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "probabilityOfUsingSelectedItem" is ' +
+      'less than 0',
+      function() {
+        this.settings.probabilityOfUsingSelectedItem = -0.123;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "probabilityOfUsingSelectedItem" is ' +
+      'greater than 1',
+      function() {
+        this.settings.probabilityOfUsingSelectedItem = 1.23;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "listOfPossibleItems" is missing',
+      function() {
+        delete this.settings.listOfPossibleItems;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "listOfPossibleItems" type is invalid',
+      function() {
+        this.settings.listOfPossibleItems = true;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "returnType" is missing',
+      function() {
+        delete this.settings.returnType;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "returnType" type is invalid',
+      function() {
+        this.settings.returnType = true;
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+
+    it(
+      'should return empty string when "returnType" is not a valid string',
+      function() {
+        this.settings.returnType = 'foobar';
+        var result = dataElementDelegate(this.settings);
+        expect(result).toBeUndefined();
+      }
+    );
+  });
+
+  describe('with valid "settings" argument', function() {
+    it(
+      'should be a value when "returnType" is "value"',
+      function() {
+        var result = dataElementDelegate(this.settings);
+        var resultIsSelectedItem = result === this.settings.selectedItem;
+        var resultIsInListOfPossibleItems =
+          this.settings.listOfPossibleItems.indexOf(result) > -1;
+        expect(
+          resultIsSelectedItem || resultIsInListOfPossibleItems
+        ).toBeTrue();
+      }
+    );
+
+    it(
+      'should be an index greater than 0 when "returnType" is "position"',
+      function() {
+        this.settings.returnType = 'position';
+        var result = dataElementDelegate(this.settings);
+        var resultIsGreaterThanOrEqualToZero = result >= 0;
+        var resultIsLessThanOrEqualToLengthOfListOfPossibleItems =
+          result <= this.settings.listOfPossibleItems.length;
+        expect(
+          resultIsGreaterThanOrEqualToZero &&
+          resultIsLessThanOrEqualToLengthOfListOfPossibleItems
+        ).toBeTrue();
+      }
+    );
+  });
+});
