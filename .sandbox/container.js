2b54825a97d6b47d56be9a8e345e02391d5b3fd9
Yuhui <yuhui@users.noreply.github.com>
2022-10-28 10:32:36 +0800

    test: update sandbox for both "position" and "value" differential items

--- .sandbox/container.js
+++ .sandbox/container.js
@@ -1,7 +1,12 @@
 module.exports = {
-  "extensions": {},
+  "extensions": {
+    "differential-privacy": {
+      "displayName": "Differential Privacy",
+      "settings": {}
+    }
+  },
   "dataElements": {
-    "Differential item": {
+    "Differential item by position": {
       "settings": {
         "selectedItem": "bar",
         "probabilityOfUsingSelectedItem": "0.75",
@@ -12,23 +17,40 @@ module.exports = {
       "forceLowerCase": false,
       "modulePath": "differential-privacy/src/lib/dataElements/differentialItem.js",
       "storageDuration": ""
+    },
+    "Differential item by value": {
+      "settings": {
+        "selectedItem": "bar",
+        "probabilityOfUsingSelectedItem": "0.75",
+        "listOfPossibleItems": "foo, bar, stamp",
+        "returnType": "value"
+      },
+      "cleanText": false,
+      "forceLowerCase": false,
+      "modulePath": "differential-privacy/src/lib/dataElements/differentialItem.js",
+      "storageDuration": ""
     }
   },
   "rules": [],
   "property": {
+    "name": "Sandbox property",
     "settings": {
       "id": "PR12345",
-      "domains": ["example.com"],
+      "domains": ["adobe.com", "example.com"],
       "undefinedVarsReturnEmpty": false
     }
   },
   "company": {
     "orgId": "ABCDEFGHIJKLMNOPQRSTUVWX@AdobeOrg"
   },
+  "environment": {
+    "id": "EN00000000000000000000000000000000",
+    "stage": "development"
+  },
   "buildInfo": {
-    "turbineVersion": "26.0.2",
-    "turbineBuildDate": "2021-06-26T13:48:43.367Z",
-    "buildDate": "2021-06-26T13:48:43.367Z",
+    "turbineVersion": "27.2.1",
+    "turbineBuildDate": "2022-10-27T15:06:49.459Z",
+    "buildDate": "2022-10-27T15:06:49.459Z",
     "environment": "development"
   }
 }
