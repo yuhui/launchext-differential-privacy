8d5255b504b785c1b03bb5aae2e9019df350687b
Yuhui <yuhui@users.noreply.github.com>
2024-02-07 22:16:22 +0800

    feat: refactor value validation to its own helper

--- src/lib/dataElements/differentialItem.js
+++ src/lib/dataElements/differentialItem.js
@@ -22,32 +22,14 @@ const {
     error: logError,
   },
 } = require('../controllers/turbine');
+const validateValue = require('../helpers/validateValue');
 
-var PROBABILITY_OF_USING_SELECTED_ITEM_ALLOWED_TYPES = [
-  '[object Number]',
-  '[object String]',
-];
-var LIST_OF_POSSIBLE_ITEMS_ALLOWED_TYPES = [
-  '[object Array]',
-  '[object String]',
-];
 var RETURN_TYPE_ALLOWED_VALUES = [
   'position',
   'value',
 ];
 
-/**
- * Returns the object's type using `Object.prototype.toString`.
- * Reference: https://stackoverflow.com/a/332429
- *
- * @param {*} obj The object to get its type.
- *
- * @returns {String} The object's type.
- */
-var objectType = function(obj) {
-  var toString = Object.prototype.toString;
-  return toString.call(obj);
-};
+const toString = Object.prototype.toString;
 
 /**
  * The differential item data element.
@@ -66,33 +48,33 @@ var objectType = function(obj) {
  * selected item itself, if the probability is met, or a random item from the
  * array. If any validation fails, a blank string is returned.
  */
-module.exports = function(settings) {
-  // validate settings.selectedItem
-  var selectedItem = settings.selectedItem;
-  if (!selectedItem) {
+module.exports = function({
+  selectedItem,
+  probabilityOfUsingSelectedItem,
+  listOfPossibleItems,
+  returnType
+} = {}) {
+  try {
+    validateValue(selectedItem);
+  } catch (e) {
     logError(`selected item ${e.message}`, selectedItem);
     return;
   }
 
-  // validate settings.probabilityOfUsingSelectedItem
-  var probabilityOfUsingSelectedItem = settings.probabilityOfUsingSelectedItem;
-  if (!probabilityOfUsingSelectedItem) {
+  try {
+    validateValue(probabilityOfUsingSelectedItem, ['number', 'string']);
+  } catch (e) {
     logError(`probability of using selected item ${e.message}`, probabilityOfUsingSelectedItem);
     return;
   }
-  var probabilityOfUsingSelectedItemType = objectType(probabilityOfUsingSelectedItem);
-  var probabilityOfUsingSelectedItemTypeIsAllowed =
-    PROBABILITY_OF_USING_SELECTED_ITEM_ALLOWED_TYPES.indexOf(
-      probabilityOfUsingSelectedItemType
-    ) === -1;
-  if (probabilityOfUsingSelectedItemTypeIsAllowed) {
-    logger.error('probability of using selected item is not a number');
-    return;
-  }
-  if (probabilityOfUsingSelectedItemType === '[object String]') {
-    probabilityOfUsingSelectedItem = Number(probabilityOfUsingSelectedItem);
-  }
-  if (!probabilityOfUsingSelectedItem) {
+  // ensure that probabilityOfUsingSelectedItem is a number (float)
+  const probabilityOfUsingSelectedItemValue =
+    toString.call(probabilityOfUsingSelectedItem) === '[object String]'
+      ? Number(probabilityOfUsingSelectedItem)
+      : probabilityOfUsingSelectedItem;
+  try {
+    validateValue(probabilityOfUsingSelectedItemValue, ['number']);
+  } catch (e) {
     logError(`probability of using selected item ${e.message}`, probabilityOfUsingSelectedItem);
     return;
   }
@@ -104,17 +86,9 @@ module.exports = function(settings) {
     return;
   }
 
-  // validate settings.listOfPossibleItems
-  var listOfPossibleItems = settings.listOfPossibleItems;
-  if (!listOfPossibleItems) {
-    logger.error('missing list of possible items');
-    return;
-  }
-  var listOfPossibleItemsType = objectType(listOfPossibleItems);
-  var listOfPossibleItemsTypeIsAllowed = LIST_OF_POSSIBLE_ITEMS_ALLOWED_TYPES.indexOf(
-    listOfPossibleItemsType
-  ) === -1;
-  if (listOfPossibleItemsTypeIsAllowed) {
+  try {
+    validateValue(listOfPossibleItems, ['array', 'string']);
+  } catch (e) {
     logError(`list of possible items ${e.message}`, listOfPossibleItems);
     return;
   }
